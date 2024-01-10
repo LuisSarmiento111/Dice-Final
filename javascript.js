10a10d73e888f3b0243468cad992ad38046ae48e
LuisSarmiento111 <114007214+LuisSarmiento111@users.noreply.github.com>
2024-01-10 15:11:13 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
 const rollDiceButton = document.getElementById("rollDiceButton")
+const confirmButton = document.getElementById("confirmButton")
+const resetButton = document.getElementById("resetButton")
+const form = document.getElementById("form")
 const die1 = document.getElementById("die1");
 const die2 = document.getElementById("die2");
 const die3 = document.getElementById("die3");
@@ -8,7 +11,33 @@ const die3Image = document.getElementById("die3Image");
 const total = document.getElementById("total");
 
 rollDiceButton.addEventListener("click", diceRoll);
+confirmButton.addEventListener("click", disableForm)
+resetButton.addEventListener("click",enableForm)
 
+let diceNum = "0"
+
+function disableForm() {
+    let elements = form.diceNum;
+    for (let i = 0, len = elements.length; i < len; ++i) {
+        if(elements[i].checked) {
+            diceNum = elements[i].value
+            console.log("checked")
+        }
+        elements[i].disabled = true;
+        console.log(elements[i].value)
+        }
+        console.log("diceNum: " + diceNum)
+}
+
+ function enableForm() {
+    let elements = form.diceNum;
+    for (let i = 0, len = elements.length; i < len; ++i) {
+        elements[i].disabled = false;
+        elements[i].value = ""
+        elements[i].checked = false
+        }
+        diceNum = "0"
+    }
 
 function rollDice() {
     let num = document.getElementById("rollNum").value;
@@ -19,13 +48,12 @@ function rollDice() {
 }
 
 function diceRoll() {
-    die1.innerHTML = rollDie();
-    die2.innerHTML = rollDie();
-    die3.innerHTML = rollDie();
-    die1Image.src = "images/" + parseInt(die1.innerHTML) + ".png"
-    die2Image.src = "images/" + parseInt(die2.innerHTML) + ".png"
-    die3Image.src = "images/" + parseInt(die3.innerHTML) + ".png"
-    return parseInt(die1.innerHTML) + parseInt(die2.innerHTML) + parseInt(die3.innerHTML)
+    for (let i = 1; i < diceNum + 1; i++) {
+        console.log(i)
+        let  die = "die" + i;
+        die.innerHTML = rollDice();
+        die.src = "images/" + parseInt(die.innerHTML) + ".png"
+    }
 }
 
 function rollDie() {
